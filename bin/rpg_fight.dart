129293f3e6eb120e05741759924c5a4f759e843b
Otavio <oaalves@outlook.com.br>
2024-03-12 19:37:12 -0300

    Em 'Main' necessita do finalizador do jogo ao 'vida = 0'

--- bin/rpg_fight.dart
+++ bin/rpg_fight.dart
@@ -63,87 +63,71 @@ void main() async {
 
 //VERIFICAR O PORQUE NÃO ESTÁ PARANDO COM O MENU AO CHEGAR A 0 DE VIDA.
 
-  while (vida! >= 0 || vida2! >= 0) {
+  while (true) {
     ////////////////////////////////////////////////////////////
     /// Menu Principal para a escolha de ação ao Jogardo 1
     ////////////////////////////////////////////////////////////
-    print('////////////// Escolha uma Ação //////////////\n' //
-        '1 - Escolher Carta na mão \n' //
-        '2 - Olhar Cartas na mão \n' //
-        '3 - Olhar Cartas no Cenário \n' //
-        '4 - Atacar uma Carta no Cenário \n'); //
-    ////////////////////////////////////////////////////////////
 
-    exitAction;
-    String? acaoEscolha = stdin.readLineSync();
-    var acaoEscolha1 = int.parse('$acaoEscolha');
-    bool retornarMenu = true;
-    exitAction;
-    switch (acaoEscolha1) {
-      case 1:
-        while (retornarMenu == true) {
-          print('Escolha sua carta!');
-          String? escolha = stdin.readLineSync();
-          var escolha1 = int.parse('$escolha');
-          ladodaMesa.selecionarCarta(decks0, escolha1 - 1);
-          ladodaMesa.mostrarEscolha();
+    if (vida! > 0 || vida2! > 0) {
+      print('////////////// Escolha uma Ação //////////////\n' //
+          '1 - Escolher Carta na mão \n' //
+          '2 - Olhar Cartas na mão \n' //
+          '3 - Olhar Cartas no Cenário \n' //
+          '4 - Atacar uma Carta no Cenário \n'); //
+      ////////////////////////////////////////////////////////////
 
-          print('Jogador 2 Escolhendo Carta....');
-          await Future.delayed(Duration(seconds: 5));
-          //sleep(Duration(seconds: 2));
-          Random jogador2 = Random();
-          var cartaJogador2 = jogador2.nextInt(decks1.length);
-          ladodaMesa1.selecionarCarta(decks1, cartaJogador2);
-          retornarMenu = false;
-        }
-      case 2:
-        criarPlayer.mostrarCartas();
-        exitAction;
-      case 3:
-        criarPlayer.mostrarCenario(ladodaMesa.cenarioAtivo);
-        criarPlayer.mostrarCenario(ladodaMesa1.cenarioAtivo);
-        exitAction;
-      case 4:
-        print('Esolha sua carta para Atacar, e selecione a Carta do Oponente.\n'
-            'Sua Carta: ');
-        String? cartaAtaque = stdin.readLineSync();
-        int cartaAtaque1 = int.parse('$cartaAtaque');
-        print('Escolha a Carta do Oponente: \n'
-            'Carta Oponente: ');
-        String? cartaSofre = stdin.readLineSync();
-        int cartaSofre1 = int.parse('$cartaSofre');
-        print('${cartaAtaque1.runtimeType}');
+      exitAction;
+      String? acaoEscolha = stdin.readLineSync();
+      int acaoEscolha1 = int.parse('$acaoEscolha');
+      bool retornarMenu = true;
+      exitAction;
+      switch (acaoEscolha1) {
+        case 1:
+          while (retornarMenu == true) {
+            print('Escolha sua carta!');
+            String? escolha = stdin.readLineSync();
+            var escolha1 = int.parse('$escolha');
+            ladodaMesa.selecionarCarta(decks0, escolha1 - 1);
+            ladodaMesa.mostrarEscolha();
 
-        ////RESOLVER LINHA SEGUINTE, DIZ NAO SER INT NO METODO ATACARCART EM CENARIO
-        vida2 = ladodaMesa.atacarCarta(
-            'jogardo1',
-            ladodaMesa.cenarioAtivo,
-            ladodaMesa1.cenarioAtivo,
-            cartaAtaque1,
-            cartaSofre1,
-            vida,
-            defesa,
-            vida2,
-            defesa2);
-
-        break;
-      default:
+            print('Jogador 2 Escolhendo Carta....');
+            await Future.delayed(Duration(seconds: 5));
+            //sleep(Duration(seconds: 2));
+            Random jogador2 = Random();
+            var cartaJogador2 = jogador2.nextInt(decks1.length);
+            ladodaMesa1.selecionarCarta(decks1, cartaJogador2);
+            retornarMenu = false;
+          }
+        case 2:
+          criarPlayer.mostrarCartas();
+          exitAction;
+        case 3:
+          criarPlayer.mostrarCenario(ladodaMesa.cenarioAtivo);
+          criarPlayer.mostrarCenario(ladodaMesa1.cenarioAtivo);
+          exitAction;
+        case 4:
+          print(
+              'Esolha sua carta para Atacar, e selecione a Carta do Oponente.\n'
+              'Sua Carta: ');
+          String? cartaAtaque = stdin.readLineSync();
+          int cartaAtaque1 = int.parse('$cartaAtaque');
+          print('Escolha a Carta do Oponente: \n'
+              'Carta Oponente: ');
+          String? cartaSofre = stdin.readLineSync();
+          int cartaSofre1 = int.parse('$cartaSofre');
+          vida2 = ladodaMesa.atacarCarta(
+              'jogardo1',
+              ladodaMesa.cenarioAtivo,
+              ladodaMesa1.cenarioAtivo,
+              cartaAtaque1,
+              cartaSofre1,
+              vida,
+              defesa,
+              vida2,
+              defesa2);
+          break;
+        default:
+      }
     }
   }
-
-//////////////////////////////////////////////////////////////////////////////////
-//Esta Instancia seleciona 1 carta e adiciona no lado do jogar
-//em SetCenario, dentro do cenarioAtivo, que é a variavel para o lado do jogador.
-//////////////////////////////////////////////////////////////////////////////////
-
-  //print('Esolheu mais 1 para o cenario....');
-  // ladodaMesa.selecionarCarta(decks0, 2);
-  // print('Lado da mesa Player 1 com 2 cartass $ladoplayer1');
-  // ladodaMesa.removerCarta(1);
-  //print('Lado da mesa Player 1 removido 1 carta $ladoplayer1');
-
-  // var ladoplayer2 = ladodaMesa1.selecionarCarta(decks1, 3);
-  // print('Lado da mesa Player 2 $ladoplayer2');
-
-  // print('$decks0 \n Deck0 \n \n $decks1');
 }
