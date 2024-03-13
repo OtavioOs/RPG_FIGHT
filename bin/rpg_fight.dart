86c4c36216966bcba87ed953bca970412e7fb44a
Otavio <oaalves@outlook.com.br>
2024-03-13 07:33:29 -0300

    Adicionado fim do jogo

--- bin/rpg_fight.dart
+++ bin/rpg_fight.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:test/test.dart';
+
 import 'CenarioSet.dart';
 import 'player_set.dart';
 import 'func_geral.dart';
@@ -37,6 +39,7 @@ void main() async {
   var cenar = funcCenario();
   var decks0 = criarPlayer.deckPlayer;
   var decks1 = criarPlayer2.deckPlayer;
+  var nav1 = true;
 
   var exitAction = await Future.delayed(Duration(seconds: 2));
 
@@ -66,74 +69,94 @@ void main() async {
   print('                 ██    ██  ██    ██  ██');
   print('                 ██    ██  ██    ██  ██');
   print('                 ████████  ████████  ██  ████');
+  print('                 ████      ██        ██    ██');
   print('                 ██  ██    ██        ██    ██');
-  print('                 ██   ██   ██        ██    ██');
   print('                 ██    ██  ██        ████████');
-  while (true) {
+  while (nav1 == true && vida2! > 0) {
     ////////////////////////////////////////////////////////////
     /// Menu Principal para a escolha de ação ao Jogardo 1
     ////////////////////////////////////////////////////////////
-
-    if (vida! > 0 || vida2! > 0) {
       print('////////////// Escolha uma Ação //////////////\n' //
           '1 - Escolher Carta na mão \n' //
           '2 - Olhar Cartas na mão \n' //
           '3 - Olhar Cartas no Cenário \n' //
-          '4 - Atacar uma Carta no Cenário \n'); //
+          '4 - Atacar uma Carta no Cenário \n'
+          '5 - Sair \n'); //
       ////////////////////////////////////////////////////////////
 
       exitAction;
       String? acaoEscolha = stdin.readLineSync();
-      int acaoEscolha1 = int.parse('$acaoEscolha');
-      bool retornarMenu = true;
-      exitAction;
-      switch (acaoEscolha1) {
-        case 1:
-          while (retornarMenu == true) {
-            print('Escolha sua carta!');
-            String? escolha = stdin.readLineSync();
-            var escolha1 = int.parse('$escolha');
-            ladodaMesa.selecionarCarta(decks0, escolha1 - 1);
-            ladodaMesa.mostrarEscolha();
+      
+      if (acaoEscolha == null || acaoEscolha.isEmpty) {
+        print('Escolha uma opçao!');
+        sleep(Duration(seconds: 2));
+      } else if (acaoEscolha.contains(RegExp(r'[a-zA-Z]')) ){
+        print('Escolha uma opção Válida!');
+        sleep(Duration(seconds: 2));
+      } else {
+        int acaoEscolha1 = int.parse('$acaoEscolha');
+        bool retornarMenu = true;
+        exitAction;
+        switch (acaoEscolha1) {
+          case 1:
+            while (retornarMenu == true) {
+              print('Escolha sua carta!');
+              String? escolha = stdin.readLineSync();
+              var escolha1 = int.parse('$escolha');
+              ladodaMesa.selecionarCarta(decks0, escolha1 - 1);
+              ladodaMesa.mostrarEscolha();
 
-            print('Jogador 2 Escolhendo Carta....');
-            await Future.delayed(Duration(seconds: 5));
-            //sleep(Duration(seconds: 2));
-            Random jogador2 = Random();
-            var cartaJogador2 = jogador2.nextInt(decks1.length);
-            ladodaMesa1.selecionarCarta(decks1, cartaJogador2);
-            retornarMenu = false;
-          }
-        case 2:
-          criarPlayer.mostrarCartas();
-          exitAction;
-        case 3:
-          criarPlayer.mostrarCenario(ladodaMesa.cenarioAtivo);
-          criarPlayer.mostrarCenario(ladodaMesa1.cenarioAtivo);
-          exitAction;
-        case 4:
-          print(
-              'Esolha sua carta para Atacar, e selecione a Carta do Oponente.\n'
-              'Sua Carta: ');
-          String? cartaAtaque = stdin.readLineSync();
-          int cartaAtaque1 = int.parse('$cartaAtaque');
-          print('Escolha a Carta do Oponente: \n'
-              'Carta Oponente: ');
-          String? cartaSofre = stdin.readLineSync();
-          int cartaSofre1 = int.parse('$cartaSofre');
-          vida2 = ladodaMesa.atacarCarta(
-              'jogardo1',
-              ladodaMesa.cenarioAtivo,
-              ladodaMesa1.cenarioAtivo,
-              cartaAtaque1,
-              cartaSofre1,
-              vida,
-              defesa,
-              vida2,
-              defesa2);
-          break;
-        default:
-      }
+              print('Jogador 2 Escolhendo Carta....');
+              await Future.delayed(Duration(seconds: 5));
+              //sleep(Duration(seconds: 2));
+              Random jogador2 = Random();
+              var cartaJogador2 = jogador2.nextInt(decks1.length);
+              ladodaMesa1.selecionarCarta(decks1, cartaJogador2);
+              retornarMenu = false;
+            }
+          case 2:
+            criarPlayer.mostrarCartas();
+            exitAction;
+          case 3:
+            criarPlayer.mostrarCenario(ladodaMesa.cenarioAtivo);
+            criarPlayer.mostrarCenario(ladodaMesa1.cenarioAtivo);
+            exitAction;
+          case 4:
+            print(
+                'Esolha sua carta para Atacar, e selecione a Carta do Oponente.\n'
+                'Sua Carta: ');
+            String? cartaAtaque = stdin.readLineSync();
+            int cartaAtaque1 = int.parse('$cartaAtaque');
+            print('Escolha a Carta do Oponente: \n'
+                'Carta Oponente: ');
+            String? cartaSofre = stdin.readLineSync();
+            int cartaSofre1 = int.parse('$cartaSofre');
+            vida2 = ladodaMesa.atacarCarta(
+                'jogardo1',
+                ladodaMesa.cenarioAtivo,
+                ladodaMesa1.cenarioAtivo,
+                cartaAtaque1,
+                cartaSofre1,
+                vida!,
+                defesa,
+                vida2,
+                defesa2);
+            case 5:
+              nav1 = false;
+              
+              break;
+          default:
+        }
     }
+    if (acaoEscolha == '5') {
+      print('Você saiu do jogo!');
+    }else if (vida2 == 0){
+      print('Fim de jogo! \n'
+            'Vencedor é $nickName');
+    }else if (vida == 0){
+      print('Fim de jogo! \n'
+            'Vencedor é Jogador2');
+    }
+
   }
 }
